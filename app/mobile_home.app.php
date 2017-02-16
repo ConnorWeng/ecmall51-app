ba04b94f3619b842058500782dae299cc7fb6a1c
Connor Weng <foamtea30@gmail.com>
2017-02-16 14:08:14 +0800

    display goods in the store whose state is open

--- app/mobile_home.app.php
+++ app/mobile_home.app.php
@@ -5,7 +5,7 @@ class Mobile_homeApp extends MallbaseApp {
         $order_by = 'goods_id';
         $goods_mod =& m('goods');
         $goods_list = $goods_mod->find(array(
-            'conditions' => 'g.default_spec != 0 and g.description is not null',
+            'conditions' => 'g.default_spec != 0 and s.state = 1 and g.description is not null',
             'fields' => 'g.goods_id, g.goods_name, g.default_image, g.price, s.store_id, s.store_name, s.see_price, s.mk_name, s.address, s.business_scope',
             'join' => 'belongs_to_store',
             'index_key' => false,
