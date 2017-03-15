c2027beafeb5d71334fc17cd943b1675eb30fc6f
Connor Weng <foamtea30@gmail.com>
2017-03-15 22:22:18 +0800

    query stores which are in the open area only

--- app/mobile_home.app.php
+++ app/mobile_home.app.php
@@ -5,13 +5,13 @@ class Mobile_homeApp extends MallbaseApp {
         $order_by = 'goods_id DESC';
         $goods_mod =& m('goods');
         $goods_list = $goods_mod->find(array(
-            'conditions' => 'g.default_spec != 0 and s.state = 1 and g.description is not null',
+            'conditions' => 'g.default_spec != 0 and s.state = 1 and g.description is not null and s.store_id in ('.implode(',', behalf_open_stores()).')',
             'fields' => 'g.goods_id, g.goods_name, g.default_image, g.price, s.store_id, s.store_name, s.see_price, s.mk_name, s.address, s.business_scope',
             'join' => 'belongs_to_store',
             'index_key' => false,
             'order' => $order_by,
-            'limit' => '5000, 500'));
-        $this->assign('goods_list', filter_behalf_open_stores($goods_list, 20));
+            'limit' => '5000, 20'));
+        $this->assign('goods_list', $goods_list);
         $this->display('mobile_home.html');
     }
 }
